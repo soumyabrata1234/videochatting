80dc8d67a09754a8588d093d685c5251b6615625
soumyabrata1234 <raysinha1729@gmail.com>
2025-06-18 10:27:08 +0530

    new update

--- src/pages/constant.js
+++ src/pages/constant.js
@@ -1,3 +1,4 @@
 
-export const APP_ID = 612091893; // Replace with your actual App ID 
-export const SERVER_SECRET = "17c9795898fc32a91196e9f80f1f1fb4"
+export const APP_ID = 1119444021; // Replace with your actual App ID 
+export const SERVER_SECRET = "ab7b7628ecc8cb85d20ba2241fc809cf";
+//ab7b7628ecc8cb85d20ba2241fc809cf
